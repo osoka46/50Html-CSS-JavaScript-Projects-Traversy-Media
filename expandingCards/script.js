1b499912534e2ee6fbb702fcdc80812fb76eb908
osoka46 <osman.bozdag@yahoo.com>
2023-09-28 07:50:48 +0200

    .

--- expandingCards/script.js
+++ expandingCards/script.js
@@ -1,10 +1,13 @@
 var elementList=document.querySelectorAll(".container .panel");
-var elementListActive=document.querySelectorAll(".active");
+
+
 for(let temp=0;temp<elementList.length;temp++) {
     const element=elementList[temp];
-    const elementA=elementListActive[temp];
 
-    elementA.classList.remove("active");
-    element.classList.add("active");
-    
+   
+    elementList[temp].addEventListener("click",function(){
+        document.querySelector(".active").classList.remove("active");
+        element.classList.add("active");
+
+    })
 }
